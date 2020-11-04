86380c876afb03fcd527867ad114c11e05a98452
Rishik Yechuri <56663764+Rishik-Yechuri@users.noreply.github.com>
2020-11-04 09:11:25 -0800

    Add project files.

---
+++ Stepper/Stepper/Game1.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Stepper
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D blank;
+        Rectangle blankRct;
+        SpriteEffects blankFx;
+
+        KeyboardState oldKb = Keyboard.GetState();
+
+        float rotation = 0;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            blankRct = new Rectangle(350, 200, 100, 100);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            blank = this.Content.Load<Texture2D>("object");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState kb = Keyboard.GetState();
+            // Allows the game to exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            // TODO: Add your update logic here
+            if (kb.IsKeyDown(Keys.Left) &&
+                !(oldKb.IsKeyDown(Keys.Left)))
+                rotation += 45;
+            else if (kb.IsKeyDown(Keys.Right) &&
+                !(oldKb.IsKeyDown(Keys.Right)))
+                rotation -= 45;
+
+            oldKb = kb;
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Gray);
+            spriteBatch.Begin();
+            // TODO: Add your drawing code here
+            spriteBatch.Draw(blank, blankRct, null, Color.White, (float)Math.PI * rotation / 180, new Vector2(50), blankFx, 0);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
